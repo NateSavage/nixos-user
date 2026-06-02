8c0b02f5a23d4c8409b34db1cfeea3a088c8683c
Nate Savage <nates@panopticom.online>
2026-06-02 09:00:06 -0400

    Updated Github SSH Key To Current Key

--- user/default.nix
+++ user/default.nix
@@ -105,8 +105,9 @@ in {
       };
     };
 
+    # add github and gitlab to system-wide known_hosts file
     programs.ssh.knownHosts = {
-      "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C5skZbW7YNcuG/F3";
+      "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
     };
 
