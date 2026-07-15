c10906c421eff0f0662ced6e77fe024e141d7b44
Nate Savage <nates@panopticom.online>
2026-07-15 17:40:16 -0400

    Added Zig As C Compiler
    - fixed bad reference to dotnet 10 sdk

--- user/software/cli/dev.nix
+++ user/software/cli/dev.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   users.users.nates.packages = with pkgs; [
-    stable.dotnetCorePackages.dotnet-sdk_10
+    stable.zig # we use this as our C compiler
+    stable.dotnet-sdk_10
     stable.git
     stable.git-lfs
     stable.just
