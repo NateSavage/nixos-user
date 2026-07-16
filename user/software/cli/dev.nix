353dcf92d8a1bac257f2d2af51781f98a56d666f
Nate Savage <nates@panopticom.online>
2026-07-15 23:01:11 -0400

    Added ripgrep Search Tool

--- user/software/cli/dev.nix
+++ user/software/cli/dev.nix
@@ -2,6 +2,7 @@
   users.users.nates.packages = with pkgs; [
     stable.zig # we use this as our C compiler
     stable.dotnet-sdk_10
+    stable.ripgrep
     stable.git
     stable.git-lfs
     stable.just
