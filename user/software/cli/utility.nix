01a103a46b49131b948f9e43c9f8fcf723e5da87
Nate Savage <nates@panopticom.online>
2026-07-21 17:17:20 -0400

    Removed Bad Package

--- user/software/cli/utility.nix
+++ user/software/cli/utility.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
   users.users.nates.packages = with pkgs; [
     stable.yazi
-    stable.diskonaut
     stable.fzf        # find files fuzzy (ff)
     stable.livegrep   # find words in file (fw)
   ];
