4cc2666a509945892baeaef325bb6bdc0fbdddfd
Nate Savage <nates@panopticom.online>
2026-07-21 17:07:57 -0400

    Added Some Useful CLI Search Tools

--- user/software/cli/utility.nix
+++ user/software/cli/utility.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   users.users.nates.packages = with pkgs; [
     stable.yazi
+    stable.diskonaut
+    stable.fzf        # find files fuzzy (ff)
+    stable.livegrep   # find words in file (fw)
   ];
 
 }
