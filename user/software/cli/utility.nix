b26d15ce185eb9aa6a23f5a9b2eda5f010068847
Nate Savage <nates@panopticom.online>
2026-07-22 08:54:05 -0400

    Added Git Plugin For Yazi

--- user/software/cli/utility.nix
+++ user/software/cli/utility.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   users.users.nates.packages = with pkgs; [
     stable.yazi
+    stable.yaziPlugins.git
     stable.fzf        # find files fuzzy (ff)
     stable.livegrep   # find words in file (fw)
     stable.magic-wormhole
